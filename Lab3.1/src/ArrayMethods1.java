cafc00de93ebd5b92cc5e01359def501688a0013
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-01-17 09:17:45 -0500

    BEYLO

--- Lab3.1/src/ArrayMethods1.java
+++ Lab3.1/src/ArrayMethods1.java
@@ -1,6 +1,8 @@
-
+//Saurabh Bansal
 public class ArrayMethods1 
 {
+	/*Side effects of all the methods is that they will all return 
+	in ascending order, and they all return Void.*/
 	public static void insertionSort(int []L1)
 	{
 		
@@ -12,5 +14,25 @@ public class ArrayMethods1
 	public static void bubbleSort(String []L1)
 	{
 		
+	}
+	
+	//Swap Methods
+	public static void swapInt(int[]x, int in1, int in2)
+	{
+		int y = x[in1];
+		x[in1] = x[in2];
+		x[in2] = y;
+	}
+	public static void swapDouble(double[]x, int in1, int in2)
+	{
+		double y = x[in1];
+		x[in1] = x[in2];
+		x[in2] = y;
+	}
+	public static void swapString(String[]x, int in1, int in2)
+	{
+		String y = x[in1];
+		x[in1] = x[in2];
+		x[in2] = y;
 	}
 }
