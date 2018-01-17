db8de209007ead22ba2d2cad63befc9a8937eefe
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-01-17 09:09:18 -0500

    SAURABH

---
+++ Lab3.1/src/ArrayMethods1.java
@@ -0,0 +1,16 @@
+
+public class ArrayMethods1 
+{
+	public static void insertionSort(int []L1)
+	{
+		
+	}
+	public static void selectionSort(double []L1)
+	{
+		
+	}
+	public static void bubbleSort(String []L1)
+	{
+		
+	}
+}
