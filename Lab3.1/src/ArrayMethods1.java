ca91fb0b0c6eda7c1056ecac27513a347031d7d2
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-01-18 09:23:51 -0500

    Hey

--- Lab3.1/src/ArrayMethods1.java
+++ Lab3.1/src/ArrayMethods1.java
@@ -1,6 +1,20 @@
 //Saurabh Bansal
 public class ArrayMethods1 
 {
+	public static void main (String [] args)
+	{
+		String [] L1 = {"g" , "b" , "a" , "c", "d", "f", "e"};
+		for (String x : L1)
+		{
+			System.out.print(x);
+		}
+		bubbleSort(L1);
+		System.out.println();
+		for (String x : L1)
+		{
+			System.out.print(x);
+		}
+	}
 	/*Side effects of all the methods is that they will all return 
 	in ascending order, and they all return Void.*/
 	public static void insertionSort(int []L1)
@@ -13,7 +27,19 @@ public class ArrayMethods1
 	}
 	public static void bubbleSort(String []L1)
 	{
-		
+		boolean f = true;
+		while (f)
+		{
+			f = false;
+			for (int x = 0; x < L1.length - 1; x++)
+			{
+				if (L1[x].compareToIgnoreCase(L1[x+1]) > 0 )
+				{
+					swapString(L1, x, x+1);
+					f = true;
+				}
+			}
+		}
 	}
 	
 	//Swap Methods
